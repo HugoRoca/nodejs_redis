2f42a8a453bb83cb8c6694a07f6e46a186e8c031
HugoRoca <hugo.rock20@hotmail.com>
2019-12-10 09:33:15 -0500

    set url

--- axiosGet.js
+++ axiosGet.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 (async () => {
     try {
-        const response = await axios.get("https://vpc-es-sbsearch-prd-a5xq7pyb6cvphjra33ojtejvwa.us-east-1.es.amazonaws.com");
+        const response = await axios.get("url");
         console.log(response);
     } catch (error) {
         console.log(`Error al procesar: ${error}`);
